e70ddd780bc1be3e650ae83deac9e3a8a38c824f
benhayehudi <bengreenberg@gmail.com>
2020-02-17 14:50:57 +0200

    docfinder

---
+++ lib/nexmo_markdown_renderer/initializers/doc_finder.rb
@@ -0,0 +1,5 @@
+DocFinder.configure do |config|
+  config.paths << "#{ENV['DOCS_BASE_PATH']}/_documentation"
+  config.paths << "#{ENV['DOCS_BASE_PATH']}/_use_cases"
+  config.paths << "#{ENV['DOCS_BASE_PATH']}/_tutorials"
+end
