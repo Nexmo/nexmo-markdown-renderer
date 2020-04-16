6b5ac6b0065b4875f8695bb40e1fdb62a5dd25c3
benhayehudi <bengreenberg@gmail.com>
2020-04-16 15:21:58 +0300

    move directory construction to abstracted methods

--- lib/nexmo_markdown_renderer/config/load_config.rb
+++ lib/nexmo_markdown_renderer/config/load_config.rb
@@ -1,8 +1,17 @@
 module Nexmo
   module Markdown
     class Config
+      def self.mounted?
+        defined?(Rails) && !Rails.application.nil?
+      end
+    
       def self.docs_base_path
-        @docs_base_path ||= (defined?(Rails) && Rails.application != nil && Rails.application.configuration.docs_base_path) || ENV.fetch('DOCS_BASE_PATH', '.')
+        (mounted? && Rails.application.configuration.docs_base_path) || '.'
+      end
+    
+      def self.load_file(file_path)
+        full_path =  "#{docs_base_path}/#{file_path}"
+        File.exist?(full_path) && YAML.load_file(full_path) || YAML.load_file("./#{file_path}") 
       end
     end
   end
