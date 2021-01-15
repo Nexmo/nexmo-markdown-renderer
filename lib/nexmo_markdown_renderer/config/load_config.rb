3285023da5da3bae92bd6d3897b9968f9d2ee960
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2021-01-15 12:01:41 +0100

    Fetch `DOCS_BASE_PATH` from `ENV`.
    It is set in the rails app anyway.

--- lib/nexmo_markdown_renderer/config/load_config.rb
+++ lib/nexmo_markdown_renderer/config/load_config.rb
@@ -2,7 +2,7 @@ module Nexmo
   module Markdown
     class Config
       def self.docs_base_path
-        @docs_base_path ||= (defined?(Rails) && Rails.application != nil && Rails.application.configuration.docs_base_path) || ENV.fetch('DOCS_BASE_PATH', '.')
+        @docs_base_path ||= ENV.fetch('DOCS_BASE_PATH', '.')
       end
     end
   end
